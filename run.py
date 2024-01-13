bd04102e2775487db237d8cf3f9eb4822e34a572
David-d-programmer <>
2024-01-13 16:34:11 +0000

    Request sales data function created

--- run.py
+++ run.py
@@ -12,8 +12,18 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('coffee_cafe')
 
-sales = SHEET.worksheet('sales')
+def request_sales_data():
+    """
+    request sales data input from the user
+    """
 
-data = sales.get_all_values()
+    print("Please enter sales data from the last market")
+    print("Data should be seven numbers, separated by commas")
+    print("Example: 11, 32, 24, 43, 33, 45, 10\n")
 
-print(data)
+    input_data = input("Enter your data here: ")
+    print(f"The data provided is {input_data}")
+
+request_sales_data()
+
+
