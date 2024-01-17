6dce465b45410e56a34bdab47b94a1fc40434272
David-d-programmer <>
2024-01-17 12:12:56 +0000

    Coffee list added to make our first two functions user friendly

--- run.py
+++ run.py
@@ -12,72 +12,143 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('coffee_cafe')
 
+COFFEE_LIST = ["Latte", "Americano", "Espresso", "Mocha", "Flat white", "Cappuccino", "Piccolo"]
+
 def request_sales_data():
     """
     Request sales data input from the user
     """
     while True:
-        print("Please enter sales data from the last market")
-        print("Data should be seven numbers, separated by commas")
+        print("Please enter your sales for today")
+
         print("Example: 11, 32, 24, 43, 33, 45, 10\n")
 
-        input_data = input("Enter your data here: ")
+       
+        input_data = []
+        for coffee in COFFEE_LIST:
+            coffee_sale = input("Enter sale value for today %s: " % coffee)
+            input_data.append(coffee_sale)
+
         
-        sales_data = input_data.split(",")
-        if data_confirmation(sales_data):
+        
+        if data_confirmation(input_data):
             print("data is accepted and confirmed!")
             break
 
-    return sales_data
+    return input_data
+
+def request_cost_data():
+    """
+    Request cost data input from the user
+    """
+    while True:
+        print("Please enter your cost")
+        print("Example: 11, 32, 24, 43, 33, 45, 10\n")
+
+       
+        input_data = []
+        for coffee in COFFEE_LIST:
+            coffee_cost = input("Enter cost value for today %s: " % coffee)
+            input_data.append(coffee_cost)
+
+        
+        
+        if data_confirmation(input_data):
+            print("data is accepted and confirmed!")
+            break
+
+    return input_data
+
 
 def data_confirmation(values):
     """
-    In the try, all string values are converted into integer.
-    also raises ValueError if string cannot be converted to integer
-    or if they are not exactly 7 values.
+    Here the length of the value imputed by the user should be thesame as the length of the 
+    coffee list, if not it will raise ValueError and give opportunity to try again.
     """
     try:
         [int(value) for value in values]
-        if len(values) != 7:
+        if len(values) != len(COFFEE_LIST):
             raise ValueError(
-                f"exactly 7 values is expected, you provided {len(values)}"
+                f"length of the coffee list is expected, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"wrong data: {e}, please try again.\n")
         return False
 
     return True
-
+    
 def update_sales_worksheet(data):
     """
-    Updating sales worksheet in the new role with the data provided
+    Update sales worksheet by adding new row with the data provided
     """
-    print("Sales worksheet updating...\n")
+    print("sales worksheet updating...\n")
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully.\n")
+    print("sales worksheet updated successfully")
 
+def update_cost_worksheet(data):
+    """
+    Update cost worksheet by adding new row with the data provided
+    """
+    print("cost worksheet updating...\n")
+    cost_worksheet = SHEET.worksheet("cost")
+    cost_worksheet.append_row(data)
+    print("cost worksheet updated successfully")
 
-def calculate_profit(sales_row):
+def calculate_profit(sales_data, cost_data):
     """
     Here we are comparing the cost with the sales of each kind of coffee 
     Profit here will be the difference between the sales and the cost
     (Profit = S.L - C.P)
     The negative numbers will mean a loss for that particular cofee
     """
-    print("Calculating the profit data...\n")
-    cost = SHEET.worksheet("cost").get_all_values()
-    cost_row = cost[-1]
-    print(cost_row)
+    profit_data = [sales_data[i] - cost_data[i] for i in range(len(sales_data))]
+    
+    return profit_data
+
+def print_profit_data(profit_data):
+    # print(profit_data)
+    for index, profit in enumerate(profit_data):
+        if profit > 0:
+            print(
+                "You are making profit of $%s by selling %s"
+                % (profit, COFFEE_LIST[index])
+            )
+        else:
+            print(
+                "You are making loss of $%s by selling %s"
+                % (profit, COFFEE_LIST[index])
+            )
+
+
+
+
 
 def main():
     """
     All program functions runs here
     """
-    data = request_sales_data()
-    sales_data = [int(numbers) for numbers in data]
+    sales_data = request_sales_data()
+    cost_data = request_cost_data()
+   
     update_sales_worksheet(sales_data)
-    calculate_profit(sales_data)
+    update_cost_worksheet(cost_data)
 
-main()
+    profit_data = calculate_profit(sales_data, cost_data)
+    update_profit_worksheet(profit_data)
+
+    print_profit_data(profit_data)
+
+    # Total sales figure for last 7 days, 30 days, custom
+
+    input("Enter the no. of days for which you want to see the data")
+    input("What data you need? sales/cost/profit?")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
 
